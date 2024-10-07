1e3b058dcca63f3eed68e6250954607199a04586
tranVyTDT <89985585+VyTruongTDTU@users.noreply.github.com>
2024-10-07 20:35:02 +0700

    init commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.10-slim-buster
+
+
+# Set environment variables
+ENV DB_USERNAME=${DB_USERNAME}
+ENV DB_PASSWORD=${DB_PASSWORD}
+ENV DB_PORT=${DB_PORT}
+ENV DB_NAME=${DB_NAME}
+ENV DB_HOST=${DB_HOST}
+
+WORKDIR /src
+
+COPY ./analytics/requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY ./analytics .
+
+CMD python app.py
--- analytics/config.py
+++ analytics/config.py
@@ -11,6 +11,8 @@ db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
 
 app = Flask(__name__)
+db_con_str = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+print('----------------------', db_con_str)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 db = SQLAlchemy(app)
